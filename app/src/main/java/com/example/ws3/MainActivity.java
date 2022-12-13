12590da178babd65add19558452c43e9a2a04313
MarinaDombrina <94761931+MarinaDombrina@users.noreply.github.com>
2022-12-14 01:12:28 +0300

    splash

--- app/src/main/java/com/example/ws3/MainActivity.java
+++ app/src/main/java/com/example/ws3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ws3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        new Handler().postDelayed(r, 3000);
     }
 }
